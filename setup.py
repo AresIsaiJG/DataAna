edabc6a2a8d6e83e6169090910dade71d7df5aa4
Ares Isai <ares.isai.jimenez.garcia@gmail.com>
2025-10-03 20:51:06 -0600

    <primera prueba

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mi_paquete',
+    version='0.1.0',
+    packages=find_packages(),
+    description='Descripción breve del paquete',
+    author='Ares',
+    author_email='ares.isai.jimenez.garcia@gmail.com',
+    url='https://github.com/AresIsaiJG/DataAna.git',
+    install_requires=[
+        # Lista de dependencias necesarias
+    ],
+)
